d06faab6a5719e84316af589882d35fa983029b9
Tawa Nicolas <tawanicolas@gmail.com>
2017-04-29 22:00:53 +0300

    Initial Commit

---
+++ Woahdude/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Woahdude
+//
+//  Created by Tawa Nicolas on 29/4/17.
+//  Copyright © 2017 Tawa Nicolas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
