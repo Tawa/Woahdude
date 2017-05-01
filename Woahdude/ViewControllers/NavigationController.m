d05813c5bd69eb7968c2064f339891ac8b5b177b
Tawa Nicolas <tawanicolas@gmail.com>
2017-05-01 10:45:35 +0300

    Add 2 missing files for SpriteKit version.

---
+++ Woahdude/ViewControllers/NavigationController.m
@@ -0,0 +1,18 @@
+//
+//  NavigationController.m
+//  Woahdude
+//
+//  Created by Tawa Nicolas on 29/4/17.
+//  Copyright © 2017 Tawa Nicolas. All rights reserved.
+//
+
+#import "NavigationController.h"
+
+@implementation NavigationController
+
+-(UIStatusBarStyle)preferredStatusBarStyle
+{
+	return UIStatusBarStyleLightContent;
+}
+
+@end
